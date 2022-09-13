bef843f3be964802a5a4ad5608157aeaed64322d
Vinay Reddy Guda <gudareddy.v@gmail.com>
2022-09-13 18:53:01 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,5 @@
+module "s3-bucket" {
+  source  = "app.terraform.io/vinay218/s3-bucket/aws"
+  version = "2.8.0"
+}
+
--- variables.tf
+++ variables.tf
@@ -7,6 +7,7 @@
 
 variable "prefix" {
   description = "This prefix will be included in the name of most resources."
+  bucket_prefix = "vinayreddyguda"
 }
 
 variable "region" {
